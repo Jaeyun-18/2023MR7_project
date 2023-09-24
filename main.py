13174537a34b4d9d7de59cc48592cf2eee83ed76
Jaeyun-18 <min070118@gmail.com>
2023-09-24 15:58:30 +0900

    원래 처럼 변환

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ import cv2
 from mp_handler import *
 import numpy as np
 
-left_landmarks = landmark_translate(True, ["S1", "E1", "W1", "H1"])
-center_landmarks = landmark_translate(
-    True, ["S1", "E1", "W1", "H1", "S2", "E2", "W2", "H2"])
+left_landmarks = landmark_translate(True, ["S1", "S2", "E1", "W1"])
+front_landmarks = landmark_translate(
+    True, ["S1", "S2", "E1", "E2", "W1", "W2", "H1", "H2"])
 
 left = PoseGetter(4, "left", left_landmarks)
-center = PoseGetter(
-    0, "center", center_landmarks)
+front = PoseGetter(0, "front", front_landmarks)
 
 
 while left.is_open():
     left_points, left_img = left.run_cycle(True)
-    center_points, center_img = center.run_cycle(True)
+    front_points, front_img = front.run_cycle(True)
 
-    cv2.imshow('center image', cv2.flip(left_img, 1))
-    cv2.imshow('left image', cv2.flip(center_img, 1))
+    S1_F = front_points[0]
+    S2_F = front_points[1]
+    E1_F = front_points[2]
+    E2_F = front_points[3]
+    W1_F = front_points[4]
+    W2_F = front_points[5]
+    H1_F = front_points[6]
+    H2_F = front_points[7]
+
+    S1_L = left_points[0]
+    S2_L = left_points[1]
+    E1_L = left_points[2]
+    W1_L = left_points[3]
+
+    cv2.imshow('left image', cv2.flip(left_img, 1))
+    cv2.imshow('front image', cv2.flip(front_img, 1))
 
     try:
-        print(left_points[1], center_points[1])
+        print(left_points[1], front_points[1])
     except:
         pass
+
     if cv2.waitKey(5) & 0xFF == 27:
         break
